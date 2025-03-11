982889945b2198d09e25c6b3b16cda68182a6e88
morelucks <luckykamshak@gmail.com>
2025-03-11 23:38:15 +0100

    keccak hashing impl

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,38 @@
 use sha2::Digest;
+use tiny_keccak::{Hasher, Keccak};
+use hex;
+// use std::io::{self, Write};
 
- fn hash(input:&str)->String{
+
+ fn s_hash(input:&str)->String{
     let mut hasher=sha2::Sha256::new();
     hasher.update(input.as_bytes());
     let result = hasher.finalize();
     format!("{:x}", result)
 
  }
+
+ fn keccak256(data: &[u8]) -> String {
+    let mut output = [0u8; 32];
+    let mut hasher = Keccak::v256();
+    hasher.update(data);
+    hasher.finalize(&mut output);
+    hex::encode(output);
+    // output
+}
+
 fn main() {
 
-let  name:&str = "this a ";
+let  name:&str = " Luckify ";
     
 println!("Input: {}", name);
-let hash_name =hash(name);
 
-println!("Input: {}", hash_name);
+let sha_hash_name =s_hash(name);
+let kec_hash_name=keccak256(name.as_bytes());
+
+
+println!("Input: {}", sha_hash_name);
+
+println!("Input: {:?}", kec_hash_name);
 
 }
